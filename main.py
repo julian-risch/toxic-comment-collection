28673c46fdadf17a7a0f41fc56c71b5c55aaa1a8
Philipp Schmidt <philipp.schmidtstudent.hpi.uni-potsdam.de>
2021-02-05 16:13:14 +0100

    add basic logging to project

--- main.py
+++ main.py
@@ -2,24 +2,97 @@ import datasets
 from datasets import hate_speech_mlma
 import os
 import shutil
+import sys
+import getopt
+import logging
 
 TEMPDIR = "./tmp"
 FILEDIR = "./files"
 
-def fetch_datasets():
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+
+def main(args):
+    try:
+        arguments = dict(getopt.getopt(args, "-r -? -t: -f:", ['reset', 'help', 'tempdir', 'filedir'])[0])
+    except:
+        logging.error("Invalid argument!")
+        print_help()
+        return -1
+
+    if ("--tempdir" in arguments or "-t" in arguments):
+        tempdir = _parse_argument(arguments, "--tempdir", "-t")
+    else: 
+        tempdir = TEMPDIR
+
+    if ("--filedir" in arguments or "-f" in arguments):
+        tempdir = _parse_argument(arguments, "--filedir", "-f")
+    else: 
+        filedir = FILEDIR
+
+    if ("--help" in arguments or "-?" in arguments):
+        print_help()
+        exit()
+    elif ("--reset" in arguments or "-r" in arguments):
+        clear_all()
+        exit()
+
+    fetch_datasets(filedir, tempdir)
+
+def print_help():
+    print("""Usage: python3 main.py [-r][-?]
+    
+-r  --reset         Reset all existing files, deletes everything in folder 'tmp' and 'files'
+-?  --help          Show this help
+-t  --tempdir XXX   Set the temp directory to XXX (standard: ./tmp)
+-f  --filedir XXX   Set the file directory to XXX (standard: ./files""")
+
+def fetch_datasets(filedir, tempdir):
     clear_all()
+    logging.info("Download Data and perform initial processing")
+    max_suffix_length = 0
     for idx, dataset in enumerate(datasets.get_datasets()):
-        clear_directory(TEMPDIR)
-        dataset.download_and_process(os.path.join(FILEDIR, str(idx)), TEMPDIR)
+        max_suffix_length = _print_progress_bar(idx, len(datasets.get_datasets()), dataset.name, max_suffix_length)
+        _clear_directory(tempdir)
+        dataset.download_and_process(os.path.join(filedir, str(idx)), tempdir)
+    _print_progress_bar(len(datasets.get_datasets()), len(datasets.get_datasets()), "Done", max_suffix_length)
+    logging.info("Done fetching Datasets")
 
 def clear_all():
-    clear_directory(TEMPDIR)
-    clear_directory(FILEDIR)
+    logging.info("Clearing all existing Data")
+    _clear_directory(TEMPDIR)
+    _clear_directory(FILEDIR)
     
-def clear_directory(directory):
+def _clear_directory(directory):
+    logging.debug("Clearing Directory: " + str(directory))
     if os.path.exists(directory):
         shutil.rmtree(directory)
     os.mkdir(directory)
 
+def _parse_argument(args, long_arg_name, short_arg_name):
+    if (long_arg_name in args):
+        if args[long_arg_name]:
+            return args[long_arg_name]
+        else:
+            logging.error("Invalid arguments!")
+            exit()
+    elif (short_arg_name in args):
+        if args[short_arg_name]:
+            return args[short_arg_name]
+        else:
+            logging.error("Invalid arguments!")
+            exit()
+    else:
+        raise ValueError("Neither long_arg_name nor short_arg_name are present in args!")
+
+def _print_progress_bar(iteration, total, suffix='', max_suffix_length=0):
+    percent = ("{0:.1f}").format(100 * (iteration / float(total)))
+    filledLength = int(50 * iteration // total)
+    bar = '█' * filledLength + '-' * (50 - filledLength)
+    suffix_string = suffix + (' ' * (max_suffix_length - len(suffix)))
+    print(f'\rProgress: [{bar}] {percent}% {suffix_string}', end = '\r')
+    if iteration == total: 
+        print()
+    return len(suffix_string)
+
 if __name__ == '__main__':
-    fetch_datasets()
+    main(sys.argv[1:])
